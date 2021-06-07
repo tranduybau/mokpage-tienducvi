3ac6f92a83898f0d1471afb5eb22e185398deaec
Tienducvi <ducvi0987@gmail.com>
2021-06-06 21:36:05 -0700

    source commit

---
+++ Project/html/js/dist/index.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// function for object
+window.onresize = function (event) {
+  viewportwidth = $(window).width();
+
+  if (viewportwidth <= 991.95) {
+    $('#collapseExample').removeClass("show");
+  } else if (viewportwidth > 992) {
+    $('#collapseExample').addClass("show");
+  }
+};
